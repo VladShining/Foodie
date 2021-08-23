ec5a113044f70c7413b5f3e3e29714490a518d43
vlad shining <vladshining@gmail.com>
2021-08-23 14:56:27 +0300

    patch scss calorie.json

--- src/app/service/update.service.ts
+++ src/app/service/update.service.ts
@@ -3,13 +3,17 @@ import { SwUpdate } from '@angular/service-worker';
 
 @Injectable()
 export class UpdateService {
-    constructor(private swUpdate: SwUpdate) {
-        if (this.swUpdate.isEnabled) {
-            this.swUpdate.available.subscribe(e => {
-                if (confirm(`Un nouveau mis à jour est disponible , voulez vous l'installer?`)) {
-                    window.location.reload();
-                }
-            });
+  constructor(private swUpdate: SwUpdate) {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        if (
+          confirm(
+            `Un nouveau mis à jour est disponible , voulez vous l'installer?`
+          )
+        ) {
+          window.location.reload();
         }
+      });
     }
+  }
 }
