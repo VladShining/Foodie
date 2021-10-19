4518c2f2d8e23c01969eec51d6dcbc43278c794b
vlad shining <vladshining@gmail.com>
2021-10-19 11:20:11 +0300

    refont main

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { LOADERS } from '../../script/load';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit, OnDestroy {
+  i!: any;
+  elem: string[] = ['', ''];
   constructor() {}
+  ngOnDestroy(): void {
+    clearInterval(this.i);
+  }
+
+  ngOnInit(): void {
+    this.i = setInterval(() => {
+      this.elem = this.random(LOADERS);
+    }, 2000);
+  }
+
   linkNca49() {
     window.open(`https://nca-49.web.app`);
   }
 
-  ngOnInit(): void {}
+  random(elem: string[][]) {
+    return elem[Math.floor(Math.random() * elem.length)];
+  }
 }
